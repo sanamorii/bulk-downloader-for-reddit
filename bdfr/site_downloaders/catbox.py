OMEGARAZER/bulk-downloader-for-reddit-x

Add Catbox downloader
Adds downloader for catbox.moe collections.

bdfrx/site_downloaders/catbox.py
@@ -1,0 +1,39 @@
+import logging
+from itertools import chain
+from typing import Optional
+
+import bs4
+from praw.models import Submission
+
+from bdfrx.exceptions import SiteDownloaderError
+from bdfrx.resource import Resource
+from bdfrx.site_authenticator import SiteAuthenticator
+from bdfrx.site_downloaders.base_downloader import BaseDownloader
+
+logger = logging.getLogger(__name__)
+
+
+class Catbox(BaseDownloader):
+    def __init__(self, post: Submission) -> None:
+        super().__init__(post)
+
+    def find_resources(self, authenticator: Optional[SiteAuthenticator] = None) -> list[Resource]:
+        links = self.get_links(self.post.url)
+        if not links:
+            raise SiteDownloaderError("Catbox parser could not find any links")
+        links = [Resource(self.post, link, Resource.retry_download(link)) for link in links]
+        return links
+
+    @staticmethod
+    def get_links(url: str) -> set[str]:
+        content = Catbox.retrieve_url(url)
+        soup = bs4.BeautifulSoup(content.text, "html.parser")
+        collection_div = soup.find("div", attrs={"class": "imagecontainer"})
+        images = collection_div.find_all("a")
+        images = [link.get("href") for link in images]
+        videos = collection_div.find_all("video")
+        videos = [link.get("src") for link in videos]
+        audios = collection_div.find_all("audio")
+        audios = [link.get("src") for link in audios]
+        resources = chain(images, videos, audios)
+        return set(resources)

bdfrx/site_downloaders/download_factory.py
@@ -3,6 +3,7 @@ import urllib.parse
 
 from bdfrx.exceptions import NotADownloadableLinkError
 from bdfrx.site_downloaders.base_downloader import BaseDownloader
+from bdfrx.site_downloaders.catbox import Catbox
 from bdfrx.site_downloaders.delay_for_reddit import DelayForReddit
 from bdfrx.site_downloaders.direct import Direct
 from bdfrx.site_downloaders.erome import Erome
@@ -34,6 +35,8 @@ class DownloadFactory:
             return Direct
         if re.match(r"erome\.com.*", sanitised_url):
             return Erome
+        if re.match(r"catbox\.moe", sanitised_url):
+            return Catbox
         if re.match(r"delayforreddit\.com", sanitised_url):
             return DelayForReddit
         if re.match(r"reddit\.com/gallery/.*", sanitised_url) or re.match(r"patreon\.com.*", sanitised_url):

tests/site_downloaders/test_catbox.py
@@ -1,0 +1,59 @@
+from unittest.mock import Mock
+
+import pytest
+
+from bdfrx.resource import Resource
+from bdfrx.site_downloaders.catbox import Catbox
+
+
+@pytest.mark.online
+@pytest.mark.parametrize(
+    ("test_url", "expected"),
+    (
+        (
+            "https://catbox.moe/c/vel5eg",
+            {
+                "https://files.catbox.moe/h2dx9k.gif",
+                "https://files.catbox.moe/bc83lg.png",
+                "https://files.catbox.moe/aq3m2a.jpeg",
+                "https://files.catbox.moe/yfk8r7.jpeg",
+                "https://files.catbox.moe/34ofbz.png",
+                "https://files.catbox.moe/xx4lcw.mp4",
+                "https://files.catbox.moe/xocd6t.mp3",
+            },
+        ),
+    ),
+)
+def test_get_links(test_url: str, expected: set[str]):
+    results = Catbox.get_links(test_url)
+    assert results == expected
+
+
+@pytest.mark.online
+@pytest.mark.slow
+@pytest.mark.parametrize(
+    ("test_url", "expected_hashes"),
+    (
+        (
+            "https://catbox.moe/c/vel5eg",
+            {
+                "014762b38e280ef3c0d000cc5f2aa386",
+                "85799edf12e20876f37286784460ad1b",
+                "c71b88c4230aa3aaad52a644fb709737",
+                "f40cffededd1929726d9cd265cc42c67",
+                "bda1f646c49607183c2450441f2ea6e8",
+                "21b48729bf9be7884999442b73887eed",
+                "0ec327259733a8276c207cc6e1b001ad",
+            },
+        ),
+    ),
+)
+def test_download_resources(test_url: str, expected_hashes: set[str]):
+    mock_download = Mock()
+    mock_download.url = test_url
+    downloader = Catbox(mock_download)
+    results = downloader.find_resources()
+    assert all(isinstance(res, Resource) for res in results)
+    [res.download() for res in results]
+    hashes = {res.hash.hexdigest() for res in results}
+    assert hashes == set(expected_hashes)

sanamorii/bulk-downloader-for-reddit

bdfr/site_downloaders/catbox.py
@@ -1,0 +1,39 @@
+import logging
+from itertools import chain
+from typing import Optional
+
+import bs4
+from praw.models import Submission
+
+from bdfr.exceptions import SiteDownloaderError
+from bdfr.resource import Resource
+from bdfr.site_authenticator import SiteAuthenticator
+from bdfr.site_downloaders.base_downloader import BaseDownloader
+
+logger = logging.getLogger(__name__)
+
+
+class Catbox(BaseDownloader):
+    def __init__(self, post: Submission) -> None:
+        super().__init__(post)
+
+    def find_resources(self, authenticator: Optional[SiteAuthenticator] = None) -> list[Resource]:
+        links = self.get_links(self.post.url)
+        if not links:
+            raise SiteDownloaderError("Catbox parser could not find any links")
+        links = [Resource(self.post, link, Resource.retry_download(link)) for link in links]
+        return links
+
+    @staticmethod
+    def get_links(url: str) -> set[str]:
+        content = Catbox.retrieve_url(url)
+        soup = bs4.BeautifulSoup(content.text, "html.parser")
+        collection_div = soup.find("div", attrs={"class": "imagecontainer"})
+        images = collection_div.find_all("a")
+        images = [link.get("href") for link in images]
+        videos = collection_div.find_all("video")
+        videos = [link.get("src") for link in videos]
+        audios = collection_div.find_all("audio")
+        audios = [link.get("src") for link in audios]
+        resources = chain(images, videos, audios)
+        return set(resources)

bdfr/site_downloaders/download_factory.py
@@ -5,6 +5,7 @@ import urllib.parse
 
 from bdfr.exceptions import NotADownloadableLinkError
 from bdfr.site_downloaders.base_downloader import BaseDownloader
+from bdfr.site_downloaders.catbox import Catbox
 from bdfr.site_downloaders.delay_for_reddit import DelayForReddit
 from bdfr.site_downloaders.direct import Direct
 from bdfr.site_downloaders.erome import Erome
@@ -36,6 +37,8 @@ class DownloadFactory:
             return Direct
         elif re.match(r"erome\.com.*", sanitised_url):
             return Erome
+        elif re.match(r"catbox\.moe", sanitised_url):
+            return Catbox
         elif re.match(r"delayforreddit\.com", sanitised_url):
             return DelayForReddit
         elif re.match(r"reddit\.com/gallery/.*", sanitised_url):

tests/site_downloaders/test_catbox.py
@@ -1,0 +1,59 @@
+from unittest.mock import Mock
+
+import pytest
+
+from bdfr.resource import Resource
+from bdfr.site_downloaders.catbox import Catbox
+
+
+@pytest.mark.online
+@pytest.mark.parametrize(
+    ("test_url", "expected"),
+    (
+        (
+            "https://catbox.moe/c/vel5eg",
+            {
+                "https://files.catbox.moe/h2dx9k.gif",
+                "https://files.catbox.moe/bc83lg.png",
+                "https://files.catbox.moe/aq3m2a.jpeg",
+                "https://files.catbox.moe/yfk8r7.jpeg",
+                "https://files.catbox.moe/34ofbz.png",
+                "https://files.catbox.moe/xx4lcw.mp4",
+                "https://files.catbox.moe/xocd6t.mp3",
+            },
+        ),
+    ),
+)
+def test_get_links(test_url: str, expected: set[str]):
+    results = Catbox.get_links(test_url)
+    assert results == expected
+
+
+@pytest.mark.online
+@pytest.mark.slow
+@pytest.mark.parametrize(
+    ("test_url", "expected_hashes"),
+    (
+        (
+            "https://catbox.moe/c/vel5eg",
+            {
+                "014762b38e280ef3c0d000cc5f2aa386",
+                "85799edf12e20876f37286784460ad1b",
+                "c71b88c4230aa3aaad52a644fb709737",
+                "f40cffededd1929726d9cd265cc42c67",
+                "bda1f646c49607183c2450441f2ea6e8",
+                "21b48729bf9be7884999442b73887eed",
+                "0ec327259733a8276c207cc6e1b001ad",
+            },
+        ),
+    ),
+)
+def test_download_resources(test_url: str, expected_hashes: set[str]):
+    mock_download = Mock()
+    mock_download.url = test_url
+    downloader = Catbox(mock_download)
+    results = downloader.find_resources()
+    assert all(isinstance(res, Resource) for res in results)
+    [res.download() for res in results]
+    hashes = {res.hash.hexdigest() for res in results}
+    assert hashes == set(expected_hashes)